d01ae6d20c08ecd0eb1457736e04fad0471b79dc
Mubarak-baka <Nassibmubarack@gmail.com>
2024-09-24 14:18:39 +0300

    Arithmetics lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
+
+
+let random = Math.ceil(Math.random() * 100) + 1; 
+
+// 3. Create a variable called `mod` set to an equation that calculates the remainder of num3 divided by num4
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4; // Remainder is 4
+
+// 4. Create a variable called `max` that finds the highest number in a set, the value returned should be 20
+let max = Math.max(1, 5, 10, 15, 20); // Finds the maximum value, which is 20
